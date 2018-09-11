0accc0d89acf2bc3a0d6c521d65d2ffbbb73f741
Xie Di <xiedidan@yeah.net>
2018-09-11 16:09:56 +0800

    dev commit - working on detector training pipeline

--- datasets/transforms.py
+++ datasets/transforms.py
@@ -4,6 +4,40 @@ import torch
 from torch.utils.data import *
 import torchvision.transforms as transforms
 
+# helper
+def intersect(box_a, box_b):
+    max_xy = np.minimum(box_a[:, 2:], box_b[2:])
+    min_xy = np.maximum(box_a[:, :2], box_b[:2])
+    inter = np.clip((max_xy - min_xy), a_min=0, a_max=np.inf)
+    return inter[:, 0] * inter[:, 1]
+
+def overlap_numpy(box_a, box_b):
+    # compute A ∩ B / A
+    inter = intersect(box_a, box_b)
+    area_a = ((box_a[:, 2]-box_a[:, 0]) *
+              (box_a[:, 3]-box_a[:, 1]))
+    
+    return inter / area_a
+
+def jaccard_numpy(box_a, box_b):
+    """Compute the jaccard overlap of two sets of boxes.  The jaccard overlap
+    is simply the intersection over union of two boxes.
+    E.g.:
+        A ∩ B / A ∪ B = A ∩ B / (area(A) + area(B) - A ∩ B)
+    Args:
+        box_a: Multiple bounding boxes, Shape: [num_boxes,4]
+        box_b: Single bounding box, Shape: [4]
+    Return:
+        jaccard overlap: Shape: [box_a.shape[0], box_a.shape[1]]
+    """
+    inter = intersect(box_a, box_b)
+    area_a = ((box_a[:, 2]-box_a[:, 0]) *
+              (box_a[:, 3]-box_a[:, 1]))  # [A,B]
+    area_b = ((box_b[2]-box_b[0]) *
+              (box_b[3]-box_b[1]))  # [A,B]
+    union = area_a + area_b - inter
+    return inter / union  # [A,B]
+
 # mod from torchvision
 class Compose(object):
     def __init__(self, transforms):
@@ -44,7 +78,7 @@ class Normalize(object):
     def __call__(self, image, gt, w, h):
         image = transforms.functional.normalize(image, self.mean, self.std)
         return image, gt, w, h
-        
+
 # resize transform
 class Resize(object):
     def __init__(self, size):
@@ -60,6 +94,88 @@ class Resize(object):
         gt = (gt[0] * ratio, gt[1])
         
         return image, gt, w, h
+
+class RandomResizedCrop(object):
+    def __init__(self, size, p=0.5, scale=(0.08, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=2, threshold=0.5):
+        self.size = size
+        self.p = p
+        self.scale = scale
+        self.ratio = ratio
+        self.interpolation = interpolation
+        self.threshold = threshold
+
+    @staticmethod
+    def get_params(w, h, scale, ratio):
+        for attempt in range(10):
+            scale_factor = random.uniform(*scale)
+            ratio_factor = random.uniform(*ratio)
+            
+            target_area = w * h * scale_factor
+            new_w = int(round(math.sqrt(target_area * ratio_factor)))
+            new_h = int(round(math.sqrt(target_area / ratio_factor)))
+
+            if random.random() < 0.5:
+                new_w, new_h = new_h, new_w
+
+            if new_w < w and new_h < h:
+                i = random.randint(0, h - new_h)
+                j = random.randint(0, w - new_w)
+
+                return i, j, h, w
+
+        # fallback
+        new_w = min(w, h)
+        i = (h - new_w) // 2
+        j = (w - new_w) // 2
+
+        return i, j, new_w, new_w
+
+    def __call__(self, images, gts, w, h):
+        if random.uniform(0., 1.) < self.p:
+            i, j, new_h, new_w = self.get_params(w, h, self.scale, self.ratio)
+            images = [transforms.functional.resized_crop(image, i, j, new_h, new_w, self.size, self.interpolation) for image in images]
+
+            # gt transform
+            offset = np.array([j, i, j, i, 0])
+            scale = np.array([self.size[0] / new_w, self.size[1] / new_h, self.size[0] / new_w, self.size[1] / new_h, 1])
+
+            new_fullframe = np.array([0, 0, self.size[0], self.size[1]])
+
+            for i, frame in enumerate(gts):
+                new_frame = (frame - offset) * scale
+
+                # remove bbox out of the frame
+                inside_mask = overlap_numpy(new_frame[:, :4], new_fullframe) - self.threshold
+                inside_mask = inside_mask > 0.
+
+                cleared_frame = np.compress(inside_mask, new_frame, axis=0)
+
+                if len(cleared_frame) == 0:
+                    cleared_frame = np.array([[0., 0., self.size[0], self.size[1], 0.]], dtype=float)
+
+                # clip into the frame
+                cleared_frame = cleared_frame.reshape(5, -1)
+                np.clip(cleared_frame[0], 0., self.size[0], out=cleared_frame[0])
+                np.clip(cleared_frame[2], 0., self.size[0], out=cleared_frame[2])
+                np.clip(cleared_frame[1], 0., self.size[1], out=cleared_frame[1])
+                np.clip(cleared_frame[3], 0., self.size[1], out=cleared_frame[3])
+                cleared_frame = cleared_frame.reshape(-1, 5)
+
+                gts[i] = cleared_frame
+        else:
+            # fall back to simple resize
+            images = transforms.Lambda(lambda frames: [transforms.Resize(self.size)(frame) for frame in frames])(images)
+
+            w_ratio = float(self.size[0]) / w
+            h_ratio = float(self.size[1]) / h
+            ratio = np.array([w_ratio, h_ratio, w_ratio, h_ratio, 1.], dtype=np.float32)
+
+            for i, frame in enumerate(gts):
+                new_frame = frame * ratio
+
+                gts[i] = new_frame
+
+        return images, gts, w, h
     
 # target transforms
 class ComposeTarget(object):
---
+++ train-detector.py
@@ -0,0 +1,216 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import sys
+import os
+import pickle
+import argparse
+import itertools
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+import torch.nn.init as init
+import torchvision
+import torchvision.transforms as transforms
+
+import caffe
+from caffe import layers as L
+from caffe import params as P
+
+from datasets.pneumonia import *
+from datasets.transforms import *
+from utils.plot import *
+
+# constants & configs
+model_path = './caffe/caffemodel/'
+prototxt_path = './caffe/prototxt/'
+loss_layer_name = 'SoftmaxWithLoss1'
+
+num_classes = 2
+snapshot_interval = 1000
+pretrained = False
+
+# spawned workers on windows take too much gmem
+number_workers = 8
+if sys.platform == 'win32':
+    number_workers = 2
+
+size = 512
+mean = [0.49043187350911405]
+std = [0.22854086980778032]
+
+# be ware - this is different from mapping in classification
+classMapping = {
+    'Normal': 0,
+    'No Lung Opacity / Not Normal': 0,
+    'Lung Opacity': 1
+}
+
+# variables
+start_epoch = 0
+best_loss = float('inf')
+
+# helper
+def get_device(device):
+    arr = device.split(':')
+
+    if len(arr) == 1:
+        device_type = 'cpu'
+        device_id = 0
+    else:
+        device_type = 'cuda'
+        device_id = int(arr[1])
+    
+    return device_type, device_id
+
+# argparser
+parser = argparse.ArgumentParser(description='Pneumonia Classifier Training')
+parser.add_argument('--lr', default=1e-6, type=float, help='learning rate')
+parser.add_argument('--end_epoch', default=200, type=float, help='epcoh to stop training')
+parser.add_argument('--batch_size', default=4, type=int, help='batch size')
+parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+parser.add_argument('--checkpoint', default='./checkpoint/checkpoint.pth', help='checkpoint file path')
+parser.add_argument('--root', default='./rsna-pneumonia-detection-challenge/', help='dataset root path')
+parser.add_argument('--device', default='cuda:0', help='device (cuda / cpu)')
+flags = parser.parse_args()
+
+# data
+trainTransform = Compose([
+    RandomResizedCrop(size=size, p=0.7, scale=(0.9, 1.), ratio=(0.9, 1/0.9)),
+    Percentage(size=size),
+    ToTensor()
+])
+
+valTransform = Compose([
+    Resize(size=size),
+    Percentage(size=size),
+    ToTensor(),
+])
+
+trainSet = PneumoniaDetectionDataset(
+    root=flags.root,
+    phase='train',
+    transform=trainTransform,
+    classMapping=classMapping,
+    num_classes=num_classes
+)
+trainLoader = torch.utils.data.DataLoader(
+    trainSet,
+    batch_size=flags.batch_size,
+    shuffle=True,
+    num_workers=number_workers,
+    collate_fn=detectionCollate,
+)
+
+valSet = PneumoniaDetectionDataset(
+    root=flags.root,
+    phase='val',
+    transform=valTransform,
+    classMapping=classMapping,
+    num_classes=num_classes
+)
+valLoader = torch.utils.data.DataLoader(
+    valSet,
+    batch_size=flags.batch_size,
+    shuffle=False,
+    num_workers=number_workers,
+    collate_fn=detectionCollate,
+)
+
+# model
+device_type, device_id = get_device(flags.device)
+
+if device_type == 'cpu':
+    caffe.set_mode_cpu()
+else:
+    caffe.set_device(device_id)
+    caffe.set_mode_gpu()
+    
+solver = caffe.get_solver(os.path.join(prototxt_path, 'solver.prototxt'))
+
+def train(epoch):
+    print('\nTraining Epoch: {}'.format(epoch))
+
+    train_loss = 0
+    batch_count = len(trainLoader)
+
+    for batch_index, (images, gts, ws, hs, ids) in enumerate(trainLoader):
+        images = images.numpy()
+        gts = gts.numpy()
+
+        # load data to caffe memory data layer
+        solver.net.set_input_arrays(images, gts)
+
+        # train
+        solver.step(1)
+
+        # get loss from net - should be a scalar
+        loss = solver.net.blobs[loss_layer_name].data[0]
+
+        train_loss += loss
+
+        print('e:{}/{}, b:{}/{}, b_l:{:.2f}, e_l:{:.2f}'.format(
+            epoch,
+            flags.end_epoch - 1,
+            batch_index,
+            batch_count - 1,
+            loss,
+            train_loss / (batch_index + 1)
+        ))
+
+def val(epoch):
+    print('\nVal')
+
+    val_loss = 0
+    batch_count = len(valLoader)
+
+    # just perfrom forward on training net
+    for batch_index, (images, gts, ws, hs, ids) in enumerate(valLoader):
+        images = images.numpy()
+        gts = gts.numpy()
+
+        # load data to caffe memory data layer
+        solver.net.set_input_arrays(images, gts)
+
+        # val
+        solver.forward()
+
+        # get loss from net - should be a scalar
+        loss = solver.net.blobs['SoftmaxWithLoss1'].data[0]
+
+        val_loss += loss
+
+        print('e:{}/{}, b:{}/{}, b_l:{:.2f}, e_l:{:.2f}'.format(
+            epoch,
+            flags.end_epoch - 1,
+            batch_index,
+            batch_count - 1,
+            loss,
+            val_loss / (batch_index + 1)
+        ))
+
+    global best_loss
+    val_loss /= batch_count
+
+    # save checkpoint
+    if val_loss < best_loss:
+        print('Saving checkpoint, best loss: {}'.format(val_loss))
+
+        if not os.path.isdir(model_path):
+            os.mkdir(model_path)
+
+        solver.net.save(os.path.join(
+            model_path,
+            'epoch_{:0>5}_loss_{:.5f}.caffemodel'.format(epoch, val_loss)
+        ))
+
+        best_loss = val_loss
+
+# ok, main loop
+if __name__ == '__main__':
+    for epoch in range(start_epoch, flags.end_epoch):
+        train(epoch)
+        val(epoch)
