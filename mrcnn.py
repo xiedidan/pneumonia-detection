10e9b39eb2ede8faa32e45e51deb03bbf84b07da
Xie Di <xiedidan@yeah.net>
2018-10-19 17:26:00 +0800

    training pipeline ok

--- mrcnn.py
+++ mrcnn.py
@@ -25,9 +25,6 @@ import torch.nn.init as init
 import torchvision
 import torchvision.transforms as transforms
 
-from datasets.pneumonia import *
-from utils.plot import *
-
 # helper
 def get_dicom_fps(dicom_dir):
     dicom_fps = glob.glob(dicom_dir + '/' + '*.dcm')
@@ -36,31 +33,44 @@ def get_dicom_fps(dicom_dir):
 
 def parse_dataset(dicom_dir, anns): 
     image_fps = get_dicom_fps(dicom_dir)
-    image_annotations = {fp: [] for fp in image_fps}
 
-    for index, row in anns.iterrows(): 
+    # filter out non-target
+    target_images = []
+
+    for index, row in anns.iterrows():
         fp = os.path.join(dicom_dir, row['patientId'] + '.dcm')
-        image_annotations[fp].append(row)
 
-    return image_fps, image_annotations 
+        if (row['Target'] == 1) and (fp not in target_images):
+            target_images.append(fp)
+
+    # create annotation list
+    image_annotations = {fp: [] for fp in target_images}
+
+    for index, row in anns.iterrows():
+        fp = os.path.join(dicom_dir, row['patientId'] + '.dcm')
+
+        if row['Target'] == 1:
+            image_annotations[fp].append(row)
+
+    return target_images, image_annotations 
 
 # argparser
 parser = argparse.ArgumentParser(description='Pneumonia Mask RCNN Pipeline')
 parser.add_argument('--lr', default=0.004, type=float, help='learning rate')
-parser.add_argument('--end_epoch', default=200, type=float, help='epcoh to stop training')
-parser.add_argument('--batch_size', default=4, type=int, help='batch size')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
-parser.add_argument('--transfer', action='store_true', help='fintune pretrained model')
 parser.add_argument('--checkpoint', default='./checkpoint/checkpoint.pth', help='checkpoint file path')
-parser.add_argument('--lock_feature', action='store_true', help='lock feature layers for baseline')
 parser.add_argument('--lib', default='../', help='Mask RCNN library location')
 parser.add_argument('--root', default='./rsna-pneumonia-detection-challenge/', help='dataset root directory')
 parser.add_argument('--device', default='cuda:0', help='device (cuda / cpu)')
+parser.add_argument('--batch_size', default=16, type=int, help='batch size')
+parser.add_argument('--steps', default=200, type=int, help='steps(batch) in each epoch')
 parser.add_argument('--plot', action='store_true', help='plot images')
 flags = parser.parse_args()
 
 # load Mask RCNN library
-sys.path.append(os.path.join(flags.lib), 'pytorch-mask-rcnn')
+print('Loading Mask RCNN library from {}'.format(flags.lib))
+
+sys.path.append(flags.lib)
 from config import Config
 import utils
 import model as modellib
@@ -81,8 +91,8 @@ class DetectorConfig(Config):
     
     # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
     # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 8
+    GPU_COUNT = 2
+    IMAGES_PER_GPU = 256
     
     BACKBONE = 'resnet50'
     
@@ -90,14 +100,13 @@ class DetectorConfig(Config):
     
     IMAGE_MIN_DIM = 256
     IMAGE_MAX_DIM = 256
-    RPN_ANCHOR_SCALES = (16, 32, 64, 128)
-    TRAIN_ROIS_PER_IMAGE = 32
+    IMAGE_PADDING = False
+    # RPN_ANCHOR_SCALES = (16, 32, 64, 128)
+    TRAIN_ROIS_PER_IMAGE = 200
     MAX_GT_INSTANCES = 4
     DETECTION_MAX_INSTANCES = 3
     DETECTION_MIN_CONFIDENCE = 0.78  ## match target distribution
     DETECTION_NMS_THRESHOLD = 0.01
-
-    STEPS_PER_EPOCH = 200
 
 config = DetectorConfig()
 config.display()
@@ -137,7 +146,7 @@ class DetectorDataset(utils.Dataset):
         # If grayscale. Convert to RGB for consistency.
         if len(image.shape) != 3 or image.shape[2] != 3:
             image = np.stack((image,) * 3, -1)
-
+  
         return image
 
     def load_mask(self, image_id):
@@ -167,34 +176,27 @@ class DetectorDataset(utils.Dataset):
         return mask.astype(np.bool), class_ids.astype(np.int32)
 
 # training dataset
-anns = pd.read_csv(os.path.join(DATA_DIR, 'stage_1_train_labels.csv'))
-anns.head()
+train_dicom_dir = os.path.join(flags.root, 'train')
+test_dicom_dir = os.path.join(flags.root, 'test')
+
+anns = pd.read_csv(os.path.join(flags.root, 'stage_1_train_labels.csv'))
+print(anns.head())
 
 image_fps, image_annotations = parse_dataset(train_dicom_dir, anns=anns)
-
-ds = pydicom.read_file(image_fps[0]) # read dicom image from filepath 
-image = ds.pixel_array # get image array
 
 # Original DICOM image size: 1024 x 1024
 ORIG_SIZE = 1024
 
+# pick 5% samples for val
 image_fps_list = list(image_fps)
 random.seed(42)
 random.shuffle(image_fps_list)
-val_size = 1500
+val_size = int(len(image_fps_list) * 0.05)
 image_fps_val = image_fps_list[:val_size]
 image_fps_train = image_fps_list[val_size:]
 
-print(len(image_fps_train), len(image_fps_val))
+print('Samples in train set: {}, val set: {}'.format(len(image_fps_train), len(image_fps_val)))
 # print(image_fps_val[:6])
-
-# prepare the training dataset
-dataset_train = DetectorDataset(image_fps_train, image_annotations, ORIG_SIZE, ORIG_SIZE)
-dataset_train.prepare()
-
-# prepare the validation dataset
-dataset_val = DetectorDataset(image_fps_val, image_annotations, ORIG_SIZE, ORIG_SIZE)
-dataset_val.prepare()
 
 # Image augmentation (light but constant)
 augmentation = iaa.Sequential([
@@ -217,38 +219,123 @@ augmentation = iaa.Sequential([
     ]),
 ])
 
-model = modellib.MaskRCNN(
-    mode='training',
-    config=config,
-    model_dir=ROOT_DIR
+# prepare the training dataset
+dataset_train = DetectorDataset(
+    image_fps_train,
+    image_annotations,
+    ORIG_SIZE,
+    ORIG_SIZE
 )
+dataset_train.prepare()
 
-# Exclude the last layers because they require a matching
-# number of classes
-model.load_weights(COCO_WEIGHTS_PATH, by_name=True, exclude=[
-    "mrcnn_class_logits", "mrcnn_bbox_fc",
-    "mrcnn_bbox", "mrcnn_mask"])
+# prepare the validation dataset
+dataset_val = DetectorDataset(
+    image_fps_val,
+    image_annotations,
+    ORIG_SIZE,
+    ORIG_SIZE
+)
+dataset_val.prepare()
 
-LEARNING_RATE = flags.lr
+model = modellib.MaskRCNN(
+    config=config,
+    model_dir=flags.lib,
+    device=device
+)
+model.to(device)
+
+coco_weights_path = os.path.join(flags.lib, 'mask_rcnn_coco.pth')
+model.load_pre_weights(coco_weights_path)
+
+# if torch.cuda.device_count() > 1:
+#    model = torch.nn.DataParallel(model)
+
+# directly read weights here and filter unmatched layers
+'''
+coco_weights = torch.load(coco_weights_path)
+unmatched_layers = [
+    'classifier.linear_class.weight',
+    'classifier.linear_class.bias',
+    'classifier.linear_bbox.weight',
+    'classifier.linear_bbox.bias',
+    'mask.conv5.weight',
+    'mask.conv5.bias'
+]
+layer_names = coco_weights.keys()
+rsna_weights = model.state_dict()
+
+for layer_name in layer_names:
+    if layer_name not in unmatched_layers:
+        rsna_weights[layer_name] = coco_weights[layer_name]
+
+model.load_state_dict(rsna_weights)
+'''
 
 # Train Mask-RCNN Model 
 ## train heads with higher lr to speedup the learning
-model.train(dataset_train, dataset_val,
-            learning_rate=flags.lr * 2,
-            epochs=2,
-            layers='heads',
-            augmentation=None)  ## no need to augment yet
+model.train_model(
+    dataset_train,
+    dataset_val,
+    learning_rate=flags.lr * 2,
+    epochs=2,
+    BatchSize=flags.batch_size,
+    steps=flags.steps,
+    layers='heads',
+    augmentation=None
+)  ## no need to augment yet
 
-model.train(dataset_train, dataset_val,
-            learning_rate=flags.lr,
-            epochs=6,
-            layers='all',
-            augmentation=augmentation)
+model.train_model(
+    dataset_train,
+    dataset_val,
+    learning_rate=flags.lr,
+    epochs=6,
+    BatchSize=flags.batch_size,
+    steps=flags.steps,
+    layers='all',
+    augmentation=augmentation
+)
 
-model.train(dataset_train, dataset_val,
-            learning_rate=flags.lr / 5,
-            epochs=16,
-            layers='all',
-            augmentation=augmentation)
+model.train_model(
+    dataset_train,
+    dataset_val,
+    learning_rate=flags.lr / 5,
+    epochs=16,
+    BatchSize=flags.batch_size,
+    steps=flags.steps,
+    layers='all',
+    augmentation=augmentation
+)
 
+'''
+# TODO : perform val for each epoch
+self.eval()
 
+epoch_aps = []
+image_ids = val_dataset.image_ids
+
+for image_id in image_ids:
+    image, image_meta, gt_class_id, gt_bbox, gt_mask = load_image_gt(
+        val_dataset,
+        self.config,
+        image_id
+    )
+
+    results = self.detect([image])
+    r = results[0]
+
+    ap = utils.compute_ap_range(
+        gt_bbox,
+        gt_class_id,
+        gt_mask,
+        r['rois'],
+        r['class_ids'],
+        r['scores'],
+        r['masks'],
+        iou_thresholds=[0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75]
+    )
+
+    epoch_aps.append(ap)
+
+self.aps.append(epoch_aps)
+print('===> Epoch {} Eval: mAP: {:.4f}'.format(epoch, np.mean(epoch_aps)))
+'''
